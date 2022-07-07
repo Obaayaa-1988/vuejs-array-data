1332afe3c956cc7bed5f7143aaf3a4d2910d47e7
Abigail <ashbella333@gmail.com>
2022-07-07 17:09:41 +0000

    for loop added

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
-new Vue({
-    el:"#create",
+// new Vue({
+//     el:"#create",
 
-    data: {
-      datas: [
-         {"id":3,"name":"Isaac Yeboah", "phone":"+233 54 241 3633","email":"isaac@walulel.com","admin_first_time":false,"avatar":"https://walulel-photos.s3.us-east-2.amazonaws.com/uploads/profile/avatars/KOGitClVgN52hzmPnewqDVsWh.jpg","organization":null,"board_access_admin":false,"country":"GH","created_at":"2020-06-09T12:42:37.000000Z"},
+//     data: {
+//       datas: [
+//          {"id":3,"name":"Isaac Yeboah", "phone":"+233 54 241 3633","email":"isaac@walulel.com","admin_first_time":false,"avatar":"https://walulel-photos.s3.us-east-2.amazonaws.com/uploads/profile/avatars/KOGitClVgN52hzmPnewqDVsWh.jpg","organization":null,"board_access_admin":false,"country":"GH","created_at":"2020-06-09T12:42:37.000000Z"},
 
-         {"id":1,"name":"Beth Taiwo", "phone":"+233 20 341 3633","email":"elizabeth@walulel.com","admin_first_time":false,"avatar":"https://walulel-photos.s3.us-east-2.amazonaws.com/uploads/profile/avatars/iHkEqfZhyOjqAi3DTaBGgcDel.jpg","organization":null,"board_access_admin":false,"country":"UK","created_at":"2022-07-05T16:58:09.000000Z"}
+//          {"id":1,"name":"Beth Taiwo", "phone":"+233 20 341 3633","email":"elizabeth@walulel.com","admin_first_time":false,"avatar":"https://walulel-photos.s3.us-east-2.amazonaws.com/uploads/profile/avatars/iHkEqfZhyOjqAi3DTaBGgcDel.jpg","organization":null,"board_access_admin":false,"country":"UK","created_at":"2022-07-05T16:58:09.000000Z"}
         
-      ]
+//       ]
+//     }
+
+// });
+
+const  datas = [
+           {"id":3,
+           "name":"Isaac Yeboah", 
+           "phone":"+233 54 241 3633",
+           "email":"isaac@walulel.com",
+           "admin_first_time":false,
+           "avatar":"https://walulel-photos.s3.us-east-2.amazonaws.com/uploads/profile/avatars/KOGitClVgN52hzmPnewqDVsWh.jpg",
+           "organization":null,"board_access_admin":false,
+           "country":"GH",
+           "created_at":"2020-06-09T12:42:37.000000Z"},
+  
+            {"id":1,
+            "name":"Beth Taiwo",
+             "phone":"+233 20 341 3633",
+             "email":"elizabeth@walulel.com",
+             "admin_first_time":false,
+             "avatar":"https://walulel-photos.s3.us-east-2.amazonaws.com/uploads/profile/avatars/iHkEqfZhyOjqAi3DTaBGgcDel.jpg",
+             "organization":null,
+             "board_access_admin":false,
+             "country":"UK",
+             "created_at":"2022-07-05T16:58:09.000000Z"}
+          
+         ]
+
+    for(let y = 0; y < datas.length; y++){
+      console.log(datas[y].id)
+
+      console.log(datas[y].name)
+
+      console.log(datas[y].phone)
+
     }
-
-});
-
